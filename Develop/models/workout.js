8875223d751508833b7d56ae7f2f805016454763
Jon Christie <jonpchristie@gmail.com>
2020-07-07 12:36:13 -0400

    updated and completed workout.js based on psuedocode and activities

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,7 +1,44 @@
 const mongoose = require('mongoose');
 
+
 const Schema = mongoose.Schema;
 
+let WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: "Exercise requires type"
+        },
+        name: {
+            type: String,
+            required: "Name is needed!",
+            trim: true
+        },
+        duration: {
+            type: Number,
+            required: "Please set the duration"
+        },
+        distance: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+
+    }]
+
+
+});
 
 const Workout = mongoose.model("workout", WorkoutSchema);
 module.exports = Workout;
